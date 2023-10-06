ed87fc8b7dc9af69fed5f95d8c1683ded3f63e97
DanTheComputerMan <firefoxmanexpert@gmail.com>
2023-10-06 02:41:28 -0700

    Made it user-friendly

--- connect-four.js
+++ connect-four.js
@@ -1,91 +1,50 @@
-/// Class implementation is significantly slower than just using functions themselves.
-EMPTY = null
-PLP = 0
-AIP = 1
-TURN = PLP;
-
-CONFIG = {
+const GAMESTATE = {}, CONFIG = {
 	columns: 7,
 	rows: 6,
 	length: 4
-}
+}, handler = {
+    get(target, property) {
+        switch (property.toLowerCase()) {
+            case "full":
+                return !BOARD[0].some(c => c === EMPTY);
+            case "over":
+                return target["full"] || target["won"];
+            case "victor":
+            case "winner":
+                if (winning_move(BOARD, AIP)) return AIP;
+                if (winning_move(BOARD, PLP)) return PLP;
+                return undefined;
+            case "won":
+                return winning_move(BOARD, AIP) || winning_move(BOARD, PLP);
+            default:
+                return target[property];
+        }
+    }
+};
 
-BOARD = [[]];
+COLORS = {
+	black: "⚫",
+	blue: "🔵",
+	brown: "🟤",
+	green: "🟢",
+	orange: "🟠",
+	purple: "🟣",
+	red: "🔴",
+	white: "⚪",
+	yellow: "🟡",
+}, GAME = new Proxy(GAMESTATE, handler);
 
-function new_game() {
-    BOARD = Array(CONFIG.rows).fill(null).map(() => Array(CONFIG.columns).fill(null));
+
+const AIP = COLORS.yellow, EMPTY = COLORS.black, PLP = COLORS.red, TURN = PLP;
+
+let BOARD = [[]];
+
+function can_play(column) {
+    return BOARD[0][column] === EMPTY;
 }
 
 function display_board() {
     return BOARD;
-}
-
-function set(prop, value) {
-    if (!Object.keys(CONFIG).includes(prop)) return false;
-    switch (prop) {
-        case "columns":
-        case "rows":
-            if (!Number.isInteger(value) || value < 3 || value < CONFIG.length) return false;
-            break;
-        case "length":
-            if (!Number.isInteger(value) || value < 3 || value < CONFIG.rows || value < CONFIG.columns) return false;
-            break;
-    }
-    CONFIG[prop] = value;
-}
-
-function set_pos(position) { // e.g. "2,7,2,1,2,4,5,5,3,5,5,6"
-    let _pieces = position.split(",");
-    if (_pieces.length > CONFIG.columns * CONFIG.rows) return false;
-    let _t = TURN;
-    for (let char = 0; char < _pieces.length; char++) {
-        play(BOARD, _pieces[+char] - 1, _t);
-        _t = +!_t;
-    }
-    return true;
-}
-
-function play(board, col, piece) { /// drop_piece renamed
-	if (board[0][col] !== EMPTY) {
-        return false;
-    }
-	for (let row = CONFIG.rows - 1; row >= 0; row--) {
-		if (board[row][col] === EMPTY) {
-			board[row][col] = piece;
-			return board;
-		}
-	}
-}
-
-function winning_move(board, piece) {
-    // Horizontal
-    for (let col = 0; col < CONFIG.columns - CONFIG.length; col++) {
-        for (let row = 0; row < CONFIG.rows; row++) {
-			if (board[row][col] !== piece) continue;
-            if (board[row][col] === board[row][col + 1] && board[row][col + 1] === board[row][col + 2] && board[row][col + 2] === board[row][col + 3]) return true;
-        }
-    }
-    // Vertical
-    for (let col = 0; col < CONFIG.columns; col++) {
-        for (let row = 0; row <= CONFIG.rows - CONFIG.length; row++) {
-			if (board[row][col] !== piece) continue;
-            if (board[row][col] === board[row + 1][col] && board[row + 1][col] === board[row + 2][col] && board[row + 2][col] === board[row + 3][col]) return true;
-        }
-    }
-    // Negative Diagonal \
-    for (let row = 0; row <= CONFIG.rows - CONFIG.length; row++) {
-        for (let col = 0; col <= CONFIG.columns - CONFIG.length; col++) {
-			if (board[row][col] !== piece) continue;
-            if (board[row][col] === board[row + 1][col + 1] && board[row + 1][col + 1] === board[row + 2][col + 2] && board[row + 2][col + 2] === board[row + 3][col + 3]) return true;
-        }
-    }
-    // Positive Diagonal /
-    for (let row = CONFIG.length - 1; row < CONFIG.rows; row++) {
-        for (let col = 0; col <= CONFIG.columns - CONFIG.length; col++) {
-			if (board[row][col] !== piece) continue;
-            if (board[row][col] === board[row - 1][col + 1] && board[row - 1][col + 1] === board[row - 2][col + 2] && board[row - 2][col + 2] === board[row - 3][col + 3]) return true;
-        }
-    }
 }
 
 function evaluate_window(window, piece) {
@@ -111,6 +70,98 @@ function evaluate_window(window, piece) {
 	}
     
     return score;
+}
+
+function get_valid_locations(board) {
+    let validLocations = [];
+    for (let col = 0; col < CONFIG.columns; col++) {
+        if (board[0][col] === EMPTY) validLocations.push(col);
+    }
+    // Move-order optimization for improved alpha beta pruning. Explores center column outwards.
+    let middle = Math.floor(validLocations.length / 2);
+    return validLocations.sort((a, b) => Math.abs(a - middle) - Math.abs(b - middle));
+}
+
+function is_terminal_node(board) {
+    return get_valid_locations(board).length === 0 || winning_move(board, AIP) || winning_move(board, PLP);
+}
+
+function minimax(board, depth, alpha, beta, maximizingPlayer) {
+    let validLocations = get_valid_locations(board), isTerminal = is_terminal_node(board);
+    if (depth === 0 || isTerminal) {
+        if (isTerminal) {
+            if (winning_move(board, AIP)) {
+                return [ null, 1000000 ];
+            } else if (winning_move(board, PLP)) {
+                return [ null, -1000000 ];
+            } else {
+                return [ null, 0 ];
+            }
+        } else {
+            return [ null, score_position(board, AIP) ];
+        }
+    }
+    if (maximizingPlayer) {
+        let value = -Infinity, column = 0;
+        for (let col of validLocations) {
+            let copy = board.map((a) => a.slice());
+            
+			play(copy, col, AIP);
+            
+            let newScore = minimax(copy, depth - 1, alpha, beta, false)[1];
+            if (newScore > value) {
+                value = newScore;
+                column = col;
+            }
+            alpha = Math.max(value, alpha);
+            if (alpha >= beta) break;
+        }
+        return [ column, value ];
+    } else {
+        let value = Infinity, column = 0;
+        for (let col of validLocations) {
+            let copy = board.map((a) => a.slice());
+            
+			play(copy, col, PLP);
+            let newScore = minimax(copy, depth - 1, alpha, beta, true)[1];
+            if (newScore < value) {
+                value = newScore;
+                column = col;
+            }
+            beta = Math.min(value, beta);
+            if (alpha >= beta) break;
+        }
+        return [ column, value ];
+    }
+}
+
+function new_game() {
+    BOARD = Array(CONFIG.rows).fill(EMPTY).map(() => Array(CONFIG.columns).fill(EMPTY));
+}
+
+function play(board, col, piece) {
+	if (board[0][col] !== EMPTY) return false;
+	for (let row = CONFIG.rows - 1; row >= 0; row--) {
+		if (board[row][col] === EMPTY) {
+			board[row][col] = piece;
+			return true;
+		}
+	}
+}
+
+function play_ai(col=false, depth=7) { 
+    if (col) return play(BOARD, col, AIP);
+    return play(BOARD, minimax(BOARD, depth, -Infinity, Infinity, true)[0], AIP);
+}
+
+function play_human(col) { 
+	if (BOARD[0][col] !== EMPTY) return false;
+	for (let row = CONFIG.rows - 1; row >= 0; row--) {
+		if (BOARD[row][col] === EMPTY) {
+			BOARD[row][col] = PLP;
+			return true;
+		}
+	}
 }
 
 function score_position(board, piece) {
@@ -163,72 +214,87 @@ function score_position(board, piece) {
     return score;
 }
 
-function is_terminal_node(board) {
-    return winning_move(board, PLP) || winning_move(board, AIP) || get_valid_locations(board).length === 0;
+function set(prop, value) {
+    if (!Object.keys(CONFIG).includes(prop)) return false;
+    switch (prop) {
+        case "columns":
+        case "rows":
+            if (!Number.isInteger(value) || value < 3 || value < CONFIG.length) return false;
+            break;
+        case "length":
+            if (!Number.isInteger(value) || value < 3 || value < CONFIG.rows || value < CONFIG.columns) return false;
+            break;
+    }
+    CONFIG[prop] = value;
+    // QOL feature.
+    if ((BOARD[CONFIG.rows] || [ EMPTY ]).every(c => c === EMPTY)) new_game();
 }
 
-function minimax(board, depth, alpha, beta, maximizingPlayer) {
-    let validLocations = get_valid_locations(board), isTerminal = is_terminal_node(board);
-    if (depth === 0 || isTerminal) {
-        if (isTerminal) {
-            if (winning_move(board, AIP)) {
-                return [ null, 1000000 ];
-            } else if (winning_move(board, PLP)) {
-                return [ null, -1000000 ];
-            } else {
-                return [ null, 0 ];
-            }
-        } else {
-            return [ null, score_position(board, AIP) ];
-        }
-    }
-    if (maximizingPlayer) {
-        let value = -Infinity, column = 0;
-        for (let col of validLocations) {
-            let copy = board.map((a) => a.slice());
-            
-			play(copy, col, AIP);
-            
-            let newScore = minimax(copy, depth - 1, alpha, beta, false)[1];
-            if (newScore > value) {
-                value = newScore;
-                column = col;
-            }
-            alpha = Math.max(value, alpha);
-            if (alpha >= beta) break;
-        }
-        return [ column, value ];
-    } else {
-        let value = Infinity, column = 0;
-        for (let col of validLocations) {
-            let copy = board.map((a) => a.slice());
-            
-			play(copy, col, PLP);
-            let newScore = minimax(copy, depth - 1, alpha, beta, true)[1];
-            if (newScore < value) {
-                value = newScore;
-                column = col;
-            }
-            beta = Math.min(value, beta);
-            if (alpha >= beta) break;
-        }
-        return [ column, value ];
-    }
+function set_ai_piece(piece) {
+    let _turn = AIP;
+    AIP = piece;
+    if (_turn === TURN) TURN = AIP;
 }
 
-function get_valid_locations(board) {
-    let validLocations = [];
+function set_empty_piece(piece) {
+    EMPTY = piece;
+}
+
+function set_player_piece(piece) {
+    let _turn = PLP;
+    PLP = piece;
+    if (_turn === TURN) TURN = PLP;
+}
+
+function set_pos(position) { // e.g. "2,7,2,1,2,4,5,5,3,5,5,6"
+    let _pieces = position.split(",");
+    if (_pieces.length > CONFIG.columns * CONFIG.rows) return false;
+    let _t = TURN;
+    for (let char = 0; char < _pieces.length; char++) {
+        play(BOARD, _pieces[+char] - 1, _t);
+        // _t = +!_t;
+        _t = (_t === AIP) ? PLP : AIP;
+    }
+    return true;
+}
+
+function winning_move(board, piece) {
+    // Horizontal
+    for (let col = 0; col < CONFIG.columns - CONFIG.length; col++) {
+        for (let row = 0; row < CONFIG.rows; row++) {
+			if (board[row][col] !== piece) continue;
+            if (board[row][col] === board[row][col + 1] && board[row][col + 1] === board[row][col + 2] && board[row][col + 2] === board[row][col + 3]) return true;
+        }
+    }
+    // Vertical
     for (let col = 0; col < CONFIG.columns; col++) {
-        if (board[0][col] === EMPTY) validLocations.push(col);
+        for (let row = 0; row <= CONFIG.rows - CONFIG.length; row++) {
+			if (board[row][col] !== piece) continue;
+            if (board[row][col] === board[row + 1][col] && board[row + 1][col] === board[row + 2][col] && board[row + 2][col] === board[row + 3][col]) return true;
+        }
     }
-    // Move-order optimization for improved alpha beta pruning. Explores center column outwards.
-    let middle = Math.floor(validLocations.length / 2);
-    return validLocations.sort((a, b) => Math.abs(a - middle) - Math.abs(b - middle));
+    // Negative Diagonal \
+    for (let row = 0; row <= CONFIG.rows - CONFIG.length; row++) {
+        for (let col = 0; col <= CONFIG.columns - CONFIG.length; col++) {
+			if (board[row][col] !== piece) continue;
+            if (board[row][col] === board[row + 1][col + 1] && board[row + 1][col + 1] === board[row + 2][col + 2] && board[row + 2][col + 2] === board[row + 3][col + 3]) return true;
+        }
+    }
+    // Positive Diagonal /
+    for (let row = CONFIG.length - 1; row < CONFIG.rows; row++) {
+        for (let col = 0; col <= CONFIG.columns - CONFIG.length; col++) {
+			if (board[row][col] !== piece) continue;
+            if (board[row][col] === board[row - 1][col + 1] && board[row - 1][col + 1] === board[row - 2][col + 2] && board[row - 2][col + 2] === board[row - 3][col + 3]) return true;
+        }
+    }
+    return false;
 }
 
 new_game();
 
-// [ col, minimaxScore ] = minimax(BOARD, 11, -Infinity, Infinity, true);
 module.exports = {
-    new_game, display_board, set, set_pos, play, winning_move, evaluate_window, score_position, is_terminal_node, minimax, get_valid_locations
+    can_play, display_board, evaluate_window, get_valid_locations, is_terminal_node, 
+    minimax, new_game, play_ai, play_human, score_position, 
+    set, set_ai_piece, set_empty_piece, set_player_piece, set_pos, 
+    winning_move, 
 };
--- readme.md
+++ readme.md
@@ -1,20 +1,67 @@
-To use this, run:
-```
-const ai = require("connectfourai");
+An NPM package for _human vs human_ or _human vs computer_ play for the game Connect 4 aka Four in a row.
+
+# **Install**
+Install this package with **`npm i connectfourai`** or **`npm install connectfourai`**.
+
+# **Setup**
+```js
+// Load the module and store it in a variable.
+const AI = require("connectfourai");
 ```
 
-To make a move for the player:
-```
-const ai = require("connectfourai");
-ai.play(BOARD, 3, PLP);
-console.table(ai.display_board());
+# **Usage**
+```js
+const AI = require("connectfourai");
+
+// To start a new game.
+AI.new_game();
+
+// Makes a move for the player. Note that columns are 0-indexed, so if you want the first column then use 0, and if you want the last column use 6.
+AI.play_human(4);
+// Makes a move for the AI.
+AI.play_ai();
+// You can also specify a column for the AI to play in.
+AI.play_ai(2);
+// You can also specify a depth to play against. The higher the number, the longer the bot will take to calculate. A depth of 7 or 8 is suggested.
+AI.play_ai(false, 8);
+
+// To check if a column can be played.
+AI.can_play(3);
+
+// To see the board.
+AI.display_board();
 ```
 
-To make a move for the AI:
+# **Built-in variables**
+For information on the state of the game.
+```js
+COLORS                          // A collection of unicode colors to choose from.
+COLORS.blue                     // One example.
+GAME.full                       // If the board is full.
+GAME.over                       // If the game is over (full or someone won).
+GAME.victor or GAME.winner      // Who won the game.
+GAME.won                        // If someone won the game.
 ```
-const ai = require("connectfourai");
 
-// Choose desired depth as 2nd argument of minimax.
-ai.play(BOARD, ai.minimax(BOARD, 7, -Infinity, Infinity, true)[0], AIP);
-console.table(ai.display_board());
+# **Customization**
+```js
+const AI = require("connectfourai");
+
+// If desired, customize the pieces. They can be anything you want!
+AI.set_ai_piece("⚪");          // Default: ⚫
+AI.set_player_piece("🔵");      // Default: 🔴
+AI.set_empty_piece("🟢");       // Default: 🟡
+
+// You can also use the provided color object.
+AI.set_ai_piece(COLORS.white);
+AI.set_player_piece(COLORS.blue);
+AI.set_empty_piece(COLORS.green);
+
+// You can change the rows, columns, and even the connect length.
+AI.set("columns", 10);          // Minimum 3
+AI.set("rows", 9);              // Minimum 3
+AI.set("length", 5);            // Minimum 3
+
+// You can setup a specific position. The board is setup by placing a piece in each column, from left-to-right.
+AI.set_pos("4,4,4,4,4,3,3,3,6");
 ```
